b01bcf86d5f0099eefbc7263d45ff5c32dfa8062
Rostyk2 <roctsk201001@gmail.com>
2024-06-15 11:38:57 +0300

    Add files via upload

---
+++ create.py
@@ -0,0 +1,38 @@
+from quizz.squel import *
+
+manager = SQLManager("kahoot_3.db")
+manager.create_tables()
+
+                       #1
+#manager.add_quizz("Україна","Квіз про Україну де розказкється про неймовірно красиву країну")
+
+                       #1
+#manager.add_answer(1,"Дніпро",True)
+#manager.add_answer(1,"Сіверський Донецьк",False)
+#manager.add_quest(1,"Яка найбільша річка  Ураїни")
+
+                        #2
+#manager.add_answer(2,"Тарас Шевченко",True)
+#manager.add_answer(2,"Іван Франко",False)
+#manager.add_quest(1,"Який найвідоміший поет України?")
+
+                        #3
+#manager.add_answer(3,"24",True)
+#manager.add_answer(3,"27",False)
+#manager.add_quest(1,"Скільки областей в Україні?")
+
+                        #4
+#manager.add_answer(4,"росія",True)
+#manager.add_answer(4,"Білорусь",False)
+#manager.add_quest(1,"Який найголовніший ворог України?")
+
+                        #5
+#manager.add_answer(5,"1991р",True)
+#manager.add_answer(5,"2014р",False)
+#manager.add_quest(1,"В якому році Україна стала незалежною?")
+
+                        #6
+#manager.add_answer(6,"Так",True)
+#manager.add_answer(6,"Ні",False)
+#manager.add_quest(1,"Вам сподобався цей Quizz?")
+
